60f9a0162a5368f0850550a3ca1cc0a4857754a0
brian ram <bramesar5@gmail.com>
2022-06-06 20:03:11 -0500

    added and modified notes.js file

---
+++ notes.js
@@ -0,0 +1,74 @@
+//async  
+//await 
+
+
+//closure
+
+//promises 
+
+
+//arrow function///
+/*    
+const arrowFunc = (e) =>{
+    return e * 2
+
+}
+
+
+console.log(arrowFunc(2))
+
+
+
+
+^^^^^^^^^^^^^^^^^^^^^^^JAVASCRIPT^^^^^^^^^^^^^^^^^^^^^^^
+
+\/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/REACT\/ \/ \/ \/ \/ \/ \/ 
+
+*/
+
+/*
+--creating a new react file with version control with github (all the starting commands)--
+
+\\\\\\react commands/////
+
+npx create-react-app myapp 
+
+
+
+
+
+\\\\\\git and github commands/////
+
+git config --global user.name "your name"
+git config --global user.email "your email"
+git init
+git status
+git add -A / git add . /  git add specificFile.js multipleFiles.html styles.css
+git commit -m "commit message"
+git log
+git branch
+git branch <branch-name>
+git checkout <branch-name>
+git remote add origin GITHUB URL HERE
+git push -u origin master
+
+1. make sure node.js is installed and npm is up to date. run the command  -- npx create-react-app myapp -- to create 
+    a new react application named myapp.
+
+2. change directory into the newly created directory and open in code editor. 
+
+3. install git on your machine via website. go to github and create new repo. in terminal run the commands
+    -- git config --global user.name "your name" --
+    -- git config --global user.email "your email" -- these are git configuration commands and are not used to 
+    log in they instead just show who made what commit on the terminal. locate and CD to the directory you want to 
+    make a repo out of and run -- git init --
+    command. run -- git status -- at any time to check if files need to be commited and overall status of the repo.
+    after making changes to the files use command -- git add -A -- to add changed files to the stating area.
+    from the staging area use command -- git commit -m "commit message" -- to commit changes. then run 
+    -- git remote add origin GITHUB URL HERE . you'll copy the githud repo link and paste it where the placeholder 
+    text is. To finally push all the commits to the guthub server use the command -- git push -u origin master --
+
+4. clean up the react file tree by deleting unwanted files and refactoring the code in the app.js and index.js 
+    files. make a components folder and add components and start building app.
+
+*/
